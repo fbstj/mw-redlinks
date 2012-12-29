feea78c9a72b2406a2ffc4557c0ea3e77d352f9e
Joe ST <joe@fbstj.net>
2012-12-29 16:56:09 +0000

    first commit

---
+++ RedLinks.i18n.magic.php
@@ -0,0 +1,8 @@
+<?php
+$magicWords = array();
+ 
+/** English (English) */
+$magicWords['en'] = array(
+        '__RLK' => array( 1, 'REDLINKS' ),
+);
+
---
+++ RedLinks.php
@@ -0,0 +1,62 @@
+<?php
+
+$wgExtensionCredits['variable'][] = array(
+    'path'           => __FILE__,
+	'name'			=> 'RedLinks',
+	'author'		=> 'Joe ST',
+	'version'		=> '0.2',
+	'description'	=> 'Counts the number of wanted articles',
+	'url'			=> 'http://coppermind.net/'
+);
+
+$wgExtensionMessagesFiles['RedLinksMagic'] = dirname(__FILE__) . '/RedLinks.i18n.magic.php';
+ 
+$wgHooks['ParserGetVariableValueSwitch'][] = 'wfMyAssignAValue';
+function wfMyAssignAValue( &$parser, &$cache, &$magicWordId, &$ret ) {
+	if ( "__RLK" != $magicWordId )
+		return false;
+	$dbr = wfGetDB( DB_SLAVE );
+
+	/*
+SELECT pl_namespace AS namespace, COUNT( * ) AS count
+FROM mw_pagelinks
+LEFT JOIN mw_page ON pl_namespace = page_namespace
+AND pl_title = page_title
+WHERE page_namespace IS NULL 
+GROUP BY pl_namespace
+	*/	
+	$res = $dbr->select(
+		array('pagelinks', 'page'),
+		array( 'ns' => 'pl_namespace', 'count'=>'COUNT(*)' ),
+		'page_namespace IS NULL',
+		__METHOD__,
+		array( 'GROUP BY' => 'pl_namespace' ),
+		array(
+			'page' => array(
+				'LEFT JOIN', array(
+					'page_namespace = pl_namespace',
+					'page_title = pl_title'
+				)
+			)
+		)
+	);
+	$out = array();
+	foreach( $res as $row )
+	{
+		$out[$row->ns] = $row->count;
+	}
+
+	$ret = $out[0];
+	return true;
+}
+ 
+$wgHooks['MagicWordwgVariableIDs'][] = 'wfMyDeclareVarIds';
+function wfMyDeclareVarIds( &$customVariableIds ) {
+        // $customVariableIds is where MediaWiki wants to store its list of custom
+        // variable IDs. We oblige by adding ours:
+        $customVariableIds[] = '__RLK';
+ 
+        // must do this or you will silence every MagicWordwgVariableIds hook
+        // registered after this!
+        return true;
+}
